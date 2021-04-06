16ddf455fdb048f3f319ac81694cb8addb28861b
Mrcodeexe <73778854+Mrcodeexe@users.noreply.github.com>
2021-04-06 16:56:33 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,20 @@
+class Paper{
+
+constructor(x,y){
+    friction:0.5
+    density:1.2
+    restitution:0.3
+    isStatic:false
+
+}
+
+
+
+
+display()
+{
+    image(this.image.x,this.image.y,this.image.width,this.image.height)
+}
+drawSprites();
+
+}
